aec17ec0f521d090be3af3e420b36e274d191e6b
berkterek <42406263+berkterek@users.noreply.github.com>
2022-09-06 13:48:43 +0300

    feat: game event added

---
+++ VoodooArmyClash/Assets/_GameFolders/Scripts/_VoodooScripts/Abstracts/GameEventHandlers/BaseGameEventListener.cs
@@ -0,0 +1,23 @@
+﻿using Voodoo.ScriptableObjects;
+using UnityEngine;
+
+namespace Voodoo.Abstracts.GameEventListeners
+{
+    public abstract class BaseGameEventListener : MonoBehaviour
+    {
+        [SerializeField] GameEvent _gameEvent;
+        
+        void OnEnable()
+        {
+            _gameEvent.AddEvent(this);
+        }
+
+        void OnDisable()
+        {
+            _gameEvent.RemoveEvent(this);
+        }
+
+        public abstract void Notify();
+        public abstract void Notify(object value);
+    }
+}
---
+++ VoodooArmyClash/Assets/_GameFolders/Scripts/_VoodooScripts/Concretes/ScriptableObjects/GameEventListeners/NormalGameEventListener.cs
@@ -0,0 +1,20 @@
+﻿using Voodoo.Abstracts.GameEventListeners;
+
+namespace Voodoo.ScriptableObjects.GameEventListeners
+{
+    public class NormalGameEventListener : BaseGameEventListener
+    {
+        public event System.Action NoParameterEvent;
+        public event System.Action<object> ParameterEventWithObject;
+
+        public override void Notify()
+        {
+            NoParameterEvent?.Invoke();
+        }
+
+        public override void Notify(object value)
+        {
+            ParameterEventWithObject?.Invoke(value);
+        }
+    }
+}
